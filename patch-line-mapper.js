8fe9625b66f548a3607c0ef01961fb92da1e5057
Mateus Craveiro <mccraveiro@gmail.com>
2017-08-25 23:19:13 -0300

    patch: clean up unnecessary code

--- patch-line-mapper.js
+++ patch-line-mapper.js
@@ -1,19 +1,18 @@
+const { assoc } = require('ramda')
+
 const getLineMapFromPatchString = (patchString) => {
-  let diffLineIndex = -1
   let fileLineIndex
 
   return patchString
     .split('\n')
-    .reduce((lineMap, line) => {
-      diffLineIndex++
-
+    .reduce((lineMap, line, diffLineIndex) => {
       if (line.match(/^@@.*/)) {
         fileLineIndex = Number(line.match(/\+([0-9]+)/)[1]) - 1
       } else if (line[0] !== '-') {
-        fileLineIndex++
+        fileLineIndex += 1
 
         if (line[0] === '+') {
-          lineMap[fileLineIndex] = diffLineIndex
+          return assoc(fileLineIndex, diffLineIndex, lineMap)
         }
       }
 
